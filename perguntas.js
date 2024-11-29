ae7b146223b610997e53b8dfaa6402d8481eecf2
Felpasz <felipe09676@gmail.com>
2024-11-29 19:48:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'One Punch-man',
+    'Quem é mais forte? Bang ou Bomb?',
+    'Bomb, quando mais jovem era mais forte do que seu irmão.'
+)
+
+criaCartao(
+    'One Punch-man',
+    'Quantos anos tem Genos?',
+    '19 anos'
+)
+
+criaCartao(
+    'One Punch-man',
+    'Qual o poder do imperador mendigo?',
+    'Criar Luzes do além'
+)
+
+criaCartao(
+    'One Punch-man',
+    'Qual a relação de Sonic com Flashy Flashy?',
+    'Eles cresceram na mesma vila ninja e foram amigos de infância'
+)
+
+criaCartao(
+    'One Punch-man',
+    'Qual é o nome do Cavaleiro sem licença?',
+    'Satoru'
+)
